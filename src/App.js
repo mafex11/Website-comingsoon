b20f021f6ffdf13fa818b81cd22122cc38bb3546
Sudhanshu pandit <sudhanshuk1140@gmail.com>
2023-12-02 21:57:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Spline from '@splinetool/react-spline';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="page1">
+      
+      <div className='spline'>
+      <Spline scene="https://prod.spline.design/6FjafyrS9ZuUawp5/scene.splinecode" />
+      </div>
+      
+      <div className="sudhanshu"></div>
+    </div>
+  );
+}
+
+/*export default App;**/
